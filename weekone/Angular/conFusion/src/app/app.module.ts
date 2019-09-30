66e4fc28650953c43dcc893e9ac4eab2fda7e4cf
sookieliao <asd6629827@gmail.com>
2019-09-29 21:01:53 -0700

    update menu template to use material-grid-list, material-card(card component) and material button.

--- weekone/Angular/conFusion/src/app/app.module.ts
+++ weekone/Angular/conFusion/src/app/app.module.ts
@@ -4,7 +4,11 @@ import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule} from '@angular/flex-layout';
 import { AppComponent } from './app.component';
-import { MatListModule} from '@angular/material/list'
+import { MatListModule} from '@angular/material/list';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatCardModule} from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 
@@ -18,7 +22,10 @@ import { MenuComponent } from './menu/menu.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
