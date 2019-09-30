7d3264a6f8288b04130132165d67df5e1818a090
sookieliao <asd6629827@gmail.com>
2019-09-29 21:52:27 -0700

    first assignment -- adding dishdetail component to display a the details for selected dish(with card) as well as the comments for this dish(with card).

--- weekone/Angular/conFusion/src/app/menu/menu.component.ts
+++ weekone/Angular/conFusion/src/app/menu/menu.component.ts
@@ -44,6 +44,7 @@ const DISHES: Dish[] = [
     description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
   }
  ];
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
