4f2d61903f3c5388247ed40fb37164d232fa2467
sookieliao <asd6629827@gmail.com>
2019-10-05 23:10:21 -0700

    use Obervables to obtain immediate change values and perform validations in typescript.

--- weekone/Angular/conFusion/src/app/contact/contact.component.ts
+++ weekone/Angular/conFusion/src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Feedback, ContactType } from '../shared/feedback';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { controlNameBinding } from '@angular/forms/src/directives/reactive_directives/form_control_name';
 
 @Component({
   selector: 'app-contact',
@@ -14,6 +15,38 @@ export class ContactComponent implements OnInit {
   contactType = ContactType;
   @ViewChild('fform') feedbackFormDirective; // this gives us access to the template form.
 
+  formErrors = {
+    'firstname':'',
+    'lastname':'',
+    'telnum':'',
+    'email':'',
+    "message":''
+  };
+
+  validationMessage = {
+    'firstname':{
+      'required':'First name is required.',
+      'minlength':'Must have at least 2 characters.',
+      'maxlength':'Can not have more than 25 characters.'
+    },
+    'lastname':{
+      'required':'Last name is required.',
+      'minlength':'Must have at least 2 characters.',
+      'maxlength':'Can not have more than 25 characters.'
+    },
+    'telnum':{
+      'required':'Telephone number is required.',
+      'pattern':'Only number is allowed.',
+    },
+    'email':{
+      'required':'Email number is required.',
+      'email':'Please enter in valid format.',
+    },
+    'message':{
+      'maxlength':'Can not have more than 225 characters.'
+    }
+  };
+
   constructor(private formBuilder: FormBuilder) {
     this.createForm();
    }
@@ -25,15 +58,24 @@ export class ContactComponent implements OnInit {
   // But we need to map this into the view/template
   createForm() {
     this.feedbackForm = this.formBuilder.group({
-      firstname: ['',Validators.required],
-      lastname: ['',Validators.required],
-      telnum: [0,Validators.required],
-      email: ['',Validators.required],
+      firstname: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      lastname: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
+      telnum: [0,[Validators.required, Validators.pattern]],
+      email: ['',[Validators.required,Validators.email]],
       agree: false,
       contacttype: 'None',
-      message: ''
+      message: ['',Validators.maxLength(255)]
     });
-    
+
+    /*
+      when the form experiences changes in any of it's form elements, 
+      Angular framework provides an observable called as valueChanges observable. 
+      We'll use the valueChanges observable on my feedback form.  
+    */
+    this.feedbackForm.valueChanges
+      .subscribe( data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set form validation messages.
   }
 
   onSubmit() {
@@ -49,6 +91,26 @@ export class ContactComponent implements OnInit {
       message: ''
     });
     this.feedbackFormDirective.resetForm(); // this will reset the template.
+  }
+
+  onValueChanged(data?: any){
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors){
+      if(this.formErrors.hasOwnProperty(field)){
+        // clear previous error message  (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessage[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] =+ messages[key]+ ' ';
+            }
+          }
+        }
+      }
+    }
   }
 }
   
