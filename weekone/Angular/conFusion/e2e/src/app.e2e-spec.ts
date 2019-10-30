b5a580756cf88e19169b50532c80e84b7d1e9ba9
sookieliao <asd6629827@gmail.com>
2019-10-29 21:37:38 -0700

    add simple end to end tests for application using library protractor.

--- weekone/Angular/conFusion/e2e/src/app.e2e-spec.ts
+++ weekone/Angular/conFusion/e2e/src/app.e2e-spec.ts
@@ -2,13 +2,21 @@ import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
   let page: AppPage;
-
+  let root: string = '/';
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to conFusion!');
+  it('should display message saying Ristorante Con Fusion', () => {
+    page.navigateTo(root);
+    expect(page.getParagraphText('app-root h1')).toEqual('should display message saying Ristorante Con Fusion');
   });
+
+  it('should navigate to about us page by clicking on the link', ()) => {
+    page.navigateTo(root);
+    // this returns all <a> tag elements
+    let navLink = page.getAllElements('a').get(1);
+    navLink.click()
+    expect(page.getParagraphText('h3')).toBe('About us');
+  }
 });
