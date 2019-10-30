62e0fdcd898ddd15cae3deaddd86c0d3686fa757
sookieliao <asd6629827@gmail.com>
2019-10-29 21:39:41 -0700

    add more e2e test.

--- weekone/Angular/conFusion/e2e/src/app.e2e-spec.ts
+++ weekone/Angular/conFusion/e2e/src/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -12,11 +13,32 @@ describe('workspace-project App', () => {
     expect(page.getParagraphText('app-root h1')).toEqual('should display message saying Ristorante Con Fusion');
   });
 
-  it('should navigate to about us page by clicking on the link', ()) => {
+  it('should navigate to about us page by clicking on the link', () => {
     page.navigateTo(root);
     // this returns all <a> tag elements
     let navLink = page.getAllElements('a').get(1);
     navLink.click()
     expect(page.getParagraphText('h3')).toBe('About us');
-  }
+  });
+
+  it('should enter a new comment for the first dish', () => {
+    // this brings us to the very first dish
+    page.navigateTo('/dishdetail/0');
+
+    // bind input web element 
+    const newAuthor = page.getElement('input[type=text');
+    
+    // this will tight value with that web element
+    newAuthor.sendKeys('Test Author');
+
+    // bind input web element for comment
+    const newComment = page.getElement('textarea');
+    newComment.sendKeys('Test Common');
+
+    const submitButton = page.getElement('button[type=submit');
+    submitButton.click();
+    
+    browser.pause()
+  });
+
 });
